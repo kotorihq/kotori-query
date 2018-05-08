e9298d3a03356d341b4c876ca01deb46bd554b3b
frohikey <frohikey@gmail.com>
2018-05-08 18:28:17 +0200

    Remove unused AtomTypeHelper, close #14

--- KotoriQuery/Tokenizer/AtomType.cs
+++ KotoriQuery/Tokenizer/AtomType.cs
@@ -27,40 +27,4 @@ namespace KotoriQuery.Tokenizer
         Or,
         Done // used
     }
-
-    public static class AtomTypeHelper 
-    {
-        private static Dictionary<string, AtomType> lookup = new Dictionary<string, AtomType> 
-        { 
-           { "Bad", AtomType.Bad },
-           { "Spaces", AtomType.Spaces },
-           { "Comma", AtomType.Comma },
-           { "Identifier", AtomType.Identifier },
-           { "Integer", AtomType.Integer },
-           { "Float", AtomType.Float },
-           { "String", AtomType.String },
-           { "Asterisk", AtomType.Asterisk },
-           { "Slash", AtomType.Slash },
-           { "Equal", AtomType.Equal },
-           { "NotEqual", AtomType.NotEqual },
-           { "LessThan", AtomType.LessThan },
-           { "LessThanThenEqual", AtomType.LessThanThenEqual },
-           { "GreaterThan", AtomType.GreaterThan },
-           { "GreaterThanThenEqual", AtomType.GreaterThanThenEqual },
-           { "OpenParenthesis", AtomType.OpenParenthesis },
-           { "CloseParenthesis", AtomType.CloseParenthesis },
-           { "Ascending", AtomType.Ascending },
-           { "Descending", AtomType.Ascending },
-           { "And", AtomType.And },
-           { "Or", AtomType.Or },
-           { "Done", AtomType.Done }
-        };
-
-        public static AtomType ToEnum(string key) 
-        {
-            AtomType ret;
-
-            return lookup.TryGetValue(key, out ret) ? ret : AtomType.Bad;
-        }
-    }
 }
